766a0da5b33c3a41359e29702b74b45c185eb7ff
BIT <BIT@DESKTOP-MIG2BMV>
2022-01-12 17:44:45 +0900

    22.01.12 DML 파일 수정

--- db/dml.sql
+++ db/dml.sql
@@ -3,7 +3,7 @@ desc emaillist;
 
 -- create
 insert into emaillist values(null,'둘','리','dooly@gmail.com');
-
+insert into emaillist values(null,'루','피','loofy@gmail.com');
 
 -- read
 select no, first_name, last_name, email from emaillist order by no desc;
